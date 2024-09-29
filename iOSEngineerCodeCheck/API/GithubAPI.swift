00d6833b34bf01ae86907a500894d366b9dbdbb1
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-29 18:08:41 +0900

    GithubAPIを追加

---
+++ iOSEngineerCodeCheck/API/GithubAPI.swift
@@ -0,0 +1,13 @@
+//
+//  GithubAPI.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 保坂篤志 on 2024/09/29.
+//  Copyright © 2024 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+class GithubAPI {
+    static let shared = GithubAPI()
+}
